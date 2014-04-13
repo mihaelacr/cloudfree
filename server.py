b095fd0f1cc4a4800f709a0b0f1e297870ea2ad8
Niklas Hambüchen <mail@nh2.me>
2014-04-13 12:41:02 +0100

    Add server

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return redirect(url_for('static', filename='index.html'))
+
+if __name__ == "__main__":
+  app.run(debug=True)

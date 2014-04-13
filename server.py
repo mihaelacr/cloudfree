2074d92e59f653cb4f7a8a26cb9b7efca550621b
Niklas Hambüchen <mail@nh2.me>
2014-04-13 12:42:26 +0100

    New index

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return redirect(url_for('static', filename='index.html'))
+  # return redirect(url_for('static', filename='index.html'))
+  return redirect(url_for('static', filename='map/map.html'))
 
 if __name__ == "__main__":
   app.run(debug=True)
